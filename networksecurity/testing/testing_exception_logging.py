8730ca6d60cb43b7372c131bfb7381f9d66cdcd9
EsmaelAwad <eawad9329@gmail.com>
2025-03-23 19:46:35 +0200

    Created the exception handling and custom logging and a testing module in which we can test everything in the project locally, tested logging exceptions and it works so far.

---
+++ networksecurity/testing/testing_exception_logging.py
@@ -0,0 +1,17 @@
+import sys
+from networksecurity.exceptions.exception import NetworkSecurityException
+from networksecurity.logging.logger import setup_logging
+
+logger = setup_logging(__name__)
+
+def divide_numbers(a, b):
+    return a / b
+
+if __name__ == "__main__":
+    try:
+        result = divide_numbers(5, 0)
+    except Exception:
+        err = NetworkSecurityException("Failed to divide numbers", sys.exc_info(), 500)
+        logger.error("An exception occurred:")
+        logger.exception(err)
+        print(err)
